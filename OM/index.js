f7fece3bdbc3b95a62a0ffdd11ed8a5dbe1e2a61
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-08-25 11:00:30 -0500

    programacion funcional

---
+++ OM/index.js
@@ -0,0 +1,44 @@
+function numerosImpares(){
+    for (i = 1; i <= 100; i += 2) {
+        console.log(i)
+    }
+}
+
+function numerosPares(){
+    let cont = 0;
+    while (cont <= 100) {
+        if (cont % 2 == 0) {
+            console.log(cont);
+        }
+        //cont = cont + 1;
+        cont++;
+    }    
+}
+
+function sumarNumeros(n1, n2){
+    let suma = 0;
+    suma = n1 + n2;
+    return suma;
+}
+
+function condicionalNumero() {
+    var numero = 2;
+    let numero1 = 1;
+    const PI = 3.1416;
+    let suma = numero + numero1
+
+    if (suma >= 5) {
+        console.log('La suma de ' + numero + ' y ' + numero1 + ' es ' + suma);
+    } else {
+        console.log(`La suma de ${numero} y ${numero1} es ${suma}`)
+    }
+}
+
+console.log("****Números Impares*****")
+numerosImpares();
+
+console.log("****Números Pares*****")
+numerosPares();
+
+let sumaDenumeros = sumarNumeros(3, 100);
+console.log(`La suma de los número es ${sumaDenumeros}`)
