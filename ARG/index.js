a87785fed509f83b9af02a42a7a2e024bad893b6
AlejoRo <alejorodg@outlook.com>
2020-09-08 12:14:53 -0500

    el jeison

--- ARG/index.js
+++ ARG/index.js
@@ -1,25 +1,21 @@
-import { PI,numerosImpares } from "./funciones/misfunciones.js";
-import moto from "./clases/moto.js";
+const express = require('express')
+const app = express()
 
-console.log("El valor de PI es:" +PI)
+app.get('/', function(req,res){
+    res.send('Hola, concejo de medellin!!!!')
+})
+app.get('/alejo', (req, res) => {
+    res.send('Hola Alo!!!')
+})
 
-console.log ("***Números Impares***")
-numerosImpares();
-
-
-let BMW = new  moto("19,400");
-var XX = new moto("30, 1000");
-
-console.log(BMW)
-console.log(XX)
-
-/*
-console.log("**Numeros Impares**");
-numerosImpares();
-
-console.log("**Numeros Pares**");
-numerosPares();
-
-let sumaDeNumeros = sumarNumeros(3, 100);
-console.log(`la suma de los números es ${sumaDeNumeros}`)
-*/
+app.get('/json', (req,res) => {
+    res.json({nombre: "Alejo", edad: "16"})
+})
+app.get('/estudiantes/:numero_estudiantes', (req, res) => {
+    console.log("el parámetro numero_estudiantes es ", req.params.numero_estudiantes)
+    res.json({numero_estudiantes:req.params.numero_estudiantes})
+})
+app.get("fibonacci/:num")
+app.listen(5057, function(){
+    console.log("Servidor corriendo en el puerto 5057")
+})
