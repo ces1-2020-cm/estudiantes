c0a88e1ed5cc1bbc3722b097ecfe0aed2e0f285a
Felipe <pipedim03@gmail.com>
2020-09-08 12:12:10 -0500

    No hagan commits pls

--- FAU/index.js
+++ FAU/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
+
 app.get('/', function(req,res){
     res.send('Hola mundo');
-    console.log(res);
+});
+name = "Felipe";
+app.listen(5057, function(){
+    console.log("El servidor está en uso.");
 });
 
-app,Listen(5057, function(){
-    console.log("El servidor está en uso.");
+app.get('/felipe', (req,res) => {
+    res.send(`hola ${name}`);
+});
+
+app.get('/json', (req,res) => {
+    res.send(`<h1>Hola señor ${name} ¿cómo se encuentra?</h1>.json`);
+});
+
+app.get('/estudiantes/:numero_estudiantes', (req,res)=>{
+    console.log("El parametro numero_estudiantes es", req.params.numero_estudiantes )
+res.json({numero_estudiantes:10})
+})
+
+app.get("/fibonacci/:num", (req, res)=>{
+
 });
