c787256dc72db2769c3a58c69f94d0bfeb868722
Snoppy0817 <Snoppy0817@gmail.com>
2020-09-08 08:42:46 -0500

    Moto, vehiculo, mis funciones

--- VCM/index.js
+++ VCM/index.js
@@ -1,46 +1,18 @@
-function condicionalNumero() {
+import {PI} from './funciones/misfunciones.js'
+import Moto from './clases/Moto.js'
 
-    var numero = 2;
-    let numero1 = 8;
-    let cont = 0;
-    const PI = 3.1416;
+console.log("El valor de PI es: "+ PI)
 
-    let suma = numero + numero1
+console.log("****Numeros Impares****")
+NumerosImpares();
 
-    if (suma >= 5) {
-        console.log(`La suma de ` + numero + ` y ` + numero1 + ` es ` + suma);
-    } else {
-        console.log(`La suma de ${numero} y ${numero1} es ${suma}`);
-    }
-}
+let BMW = new Moto(19,400);
+var XX = new Moto(30,1000);
 
-function sumarNumeros(n1, n2) {
-    let suma = 0;
-    suma = n1 + n2;
-    return suma;
-}
+console.log (BMW) 
+console.log (XX)
 
-function NumerosPares() {
-    let cont = 0;
-
-    while (cont <= 100) {
-        if (cont % 2 == 0) {
-
-            console.log(cont);
-        }
-        //cont = cont + 1;
-        cont++;
-    }
-
-}
-function NumerosImpares() {
-
-    for (i = 1; i <= 100; i += 2) {
-        console.log(i)
-    }
-
-}
-
+/*
 console.log("****Numeros Impares****")
 NumerosImpares();
 
@@ -49,3 +21,4 @@ NumerosPares();
 
 let sumaDenumeros = sumarNumeros(3,100);
 console.log (`La suma de los numeros es ${sumaDenumeros}`) 
+*/
