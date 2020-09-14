178a395b921988245714c45dbc0153d4dc15a16a
AlejoRo <alejorodg@outlook.com>
2020-09-14 14:58:49 -0500

    elfibobaci

--- ARG/index.js
+++ ARG/index.js
@@ -15,7 +15,16 @@ app.get('/estudiantes/:numero_estudiantes', (req, res) => {
     console.log("el parámetro numero_estudiantes es ", req.params.numero_estudiantes)
     res.json({numero_estudiantes:req.params.numero_estudiantes})
 })
-app.get("fibonacci/:num")
+app.get("/fibonacci", (req, res) => {
+    function fibonacci(numero){
+        let serie=[0,1];
+        for (let i = 2; i < numero; i++) {
+            serie[i] = serie[i-2] + serie[i-1];
+        }
+        return serie;
+    }
+    res.json(fibonacci(10))
+})
 app.listen(5057, function(){
     console.log("Servidor corriendo en el puerto 5057")
 })
