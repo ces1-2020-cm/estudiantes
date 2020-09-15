7d959b448a9e23c2e097ffd53c0465872cf7eccc
Juanpablobolivar <juanpablobolivarmarin0820@gmail.com>
2020-09-14 23:47:12 -0500

    Intento de la tarea sucesión de fibonacci

--- JPBM/index.js
+++ JPBM/index.js
@@ -20,8 +20,16 @@ app.get('/json', (req, res)=> {
 app.get('/estudiantes/:numero_estudiantes', (req, res) =>{
     consolelog("El parametro numero_estudiantes es", req.params)
     res.json({numero_estudiantes:10})
-})
+});
 app.get('/fibonacci/:num', (req, res)=>{
+    
+    console.log()((size =>{
+        if(size < 0) return [];
+        if(size == 0) return [0];
+        var fibonacci = [0 , 1];
+        for(i=2; i<=size; ++i) fibonacci.push(fibonacci[i-2]+fibonacci[i-1]);
+        return fibonacci;
+    }) (process.argv[2]));
 
 });
 app.listen(5057, function(){
