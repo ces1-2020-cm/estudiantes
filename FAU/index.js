7163ecb7220ef04fe4fa8ea98d8a38ba65b7c625
Felipe <pipedim03@gmail.com>
2020-09-14 23:37:29 -0500

    Intento solución Fibonacci/Fallido/By.Muertox

--- FAU/index.js
+++ FAU/index.js
@@ -26,18 +26,23 @@ res.json({numero_estudiantes:10})
 
 //let fibo_final = fibo_total;
 //app.get("/fibonacci/:num", (req, res,fibo_total) => {
+
     let fibo_origin = [0,1];
 let fibonacci = function(fibo_total){
-  let fibo_final = fibo_origin;
+  let fibo_final = fibo_origin[fibo_final];
+
 if (typeof fibo_final != 'number') {
+
     fibo_final = fibonacci(n - 1) + fibonacci(n - 2);
-    fibo_origin(fibo_total) = fibo_final;
+    
+    fibo_origin[fibo_total] = fibo_final;
 
     return fibo_final;
 }
 console.log(fibonacci(250));
+
+};
+console.log(fibonacci(250));
 console.log(`El número ${fibo_total} pertenece a la secuencia Fibonacci:`, req.params.secuencia_fibo)
 
 res.json({fibo_origin});
-
- };
