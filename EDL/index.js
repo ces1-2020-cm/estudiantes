a29dd7821f7b81d538b056f24783c98baade903a
EnmanuelDominguez <denmanuel795@gmail.com>
2020-09-17 11:36:32 -0500

    fibonacci

--- EDL/index.js
+++ EDL/index.js
@@ -12,14 +12,15 @@ app.get('/enmanuel', (req, res) =>{
 app.listen(5057, function () {
     console.log("Servidor corriendo en el puerto 5057")
 })
-import { PI, numerosImpares } from './funciones/misfunciones.js'
-import vehiculo from './clases/vehiculo.js'
-console.log("El valor de pi es: " + PI)
-
-console.log("****Número Impares****");
-numerosImpares();
-
-let bmw = new vehiculo('PO90Q', 'rojo', '2000');
-var xx = new vehiculo('RW23', 'verde');
-console.log(bmw)
-console.log(xx)
+function fibonacci(numero){
+    let a= 0, b= 1, c, s= 1;
+    console.log(a,b);
+    for(i = 3; i<=numero; i++){
+        c=a+b;
+        console.log(c);
+        s = s+c;
+        a = b;
+        b = c;
+    }
+}
+fibonacci(prompt());
