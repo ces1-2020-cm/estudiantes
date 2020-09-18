88df93eb5f1d9246aea4381d469df041e13ce559
WendyPaz <wendypazhernandez@gmail.com>
2020-09-17 19:53:09 -0500

    Fibonacci

--- WPH/index.js
+++ WPH/index.js
@@ -13,10 +13,30 @@ app.get('/json', (req, res) => {
     res.send({nombre: "Wendy",edad:17})
 })
 
+
+app.get('/fibonacci/:limite',(req,res) => {
+    function fibonacci(nums) {
+  
+        let fib = [0, 1];
+        let data = [0, 1];
+        
+        for(let i = 2; i <= nums; i++) {
+          fib[i] = fib[i - 1] + fib[i - 2]; 
+          data.push(fib[i]);
+        }
+        
+        return data;
+      }
+    res.json(fibonacci(req.params.limite))
+})
+
+
 app.get('/estudiantes/:numero_estudiantes', (req, res) => {
     console.log("El parametro numero_estudiantes es:", req.param.numero_estudiantes)
     res.json({numero_estudiantes:req.param.numero_estudiantes})
 })
+
+
 
 app.listen(5057, function(){
     console.log("Servidor corriendo en el puerto 5057")
