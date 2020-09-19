d518e7fe30a31474c2709d07a526916a4ab13714
Totoratsu <mayor.daniel.18@gmail.com>
2020-09-18 23:11:02 -0500

    Nixon fibonacci Owo

--- NDL/index.js
+++ NDL/index.js
@@ -1,8 +1,18 @@
-import { pi, nImpares } from './funciones/app';
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+/* import { pi, nImpares } from './funciones/app';
 import Moto from './clases/moto';
 
 console.log(`El valor de la constante pi es: ${pi}`);
 console.log(`###  Numeros Impares  ###\n${nImpares()}`);
 
 let susuki = new Moto(20, 375);
-console.log(susuki);
+console.log(susuki); */
+
+app.use('/', routes);
+
+app.listen(65000, () => {
+    console.log(`Server running on port ${65000}`)
+});
